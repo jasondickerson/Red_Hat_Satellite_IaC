95acfcc0abcb34d860206910b99e698880069902
Jason Dickerson <jdickers@redhat.com>
2024-02-20 17:04:57 -0600

    Add Capsule support

---
+++ capsule_prep.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Required for Capsule Installation
+# Configure Automation Hub token in ansible.cfg
+# Install redhat.satellite v4.0.0
+ansible-galaxy collection install -r collections/requirements.yml
+
